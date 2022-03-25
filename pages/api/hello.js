2629a76d21420a611697bd696d27576d9b8d9cb8
William Li <email@williamli.dev>
2022-03-25 12:42:19 +0800

    use s-maxage=60, stale-while-revalidate=2678400

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -5,6 +5,6 @@ const axios = require('axios').default
 export default async function handler(req, res) {
   const {data: response} = await axios.get('https://nextjs-time-api-1.vercel-support.app/api/time')
   console.log(response)
-  res.setHeader("Cache-Control", "s-maxage=1, stale-while-revalidate=2678400") // 2678400 seconds = 31 days
+  res.setHeader("Cache-Control", "s-maxage=60, stale-while-revalidate=2678400") // 2678400 seconds = 31 days
   res.status(200).json(response)
 }
