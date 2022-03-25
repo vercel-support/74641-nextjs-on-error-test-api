3b1321ed51861e501dd0760274bf7d21dcf14f60
William Li <email@williamli.dev>
2022-03-25 12:11:43 +0800

    fetch data from nextjs-time-api-1.vercel-support.app/api/time

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+const axios = require('axios').default
+
+export default async function handler(req, res) {
+  const {data: response} = await axios.get('https://nextjs-time-api-1.vercel-support.app/api/time')
+  console.log(response)
+  res.setHeader("Cache-Control", "s-maxage=1, stale-while-revalidate=2678400") // 2678400 seconds = 31 days
+  res.status(200).json(response)
 }
