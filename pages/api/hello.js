14d1af689c426ea935fa154851cb8b7cf94f5b83
William Li <email@williamli.dev>
2022-03-25 13:33:44 +0800

    added nextjs-time-api-3.vercel-support.app for some api test

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -3,7 +3,7 @@
 const axios = require('axios').default
 
 export default async function handler(req, res) {
-  const {data: response} = await axios.get('https://nextjs-time-api-1.vercel-support.app/api/time')
+  const {data: response} = await axios.get('https://nextjs-time-api-3.vercel-support.app/api/time')
   console.log(response)
   res.setHeader("Cache-Control", "s-maxage=60, stale-while-revalidate=2678400") // 2678400 seconds = 31 days
   res.status(200).json(response)
